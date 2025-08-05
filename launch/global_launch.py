83ea7568f31717e99252276921b7068868e45d30
Alan Papalia <alanpapalia@gmail.com>
2025-08-05 12:11:31 -0400

    set domain id in launch file

--- launch/global_launch.py
+++ launch/global_launch.py
@@ -16,6 +16,24 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
+def get_domain_id():
+    computer_hostname = os.uname()[1]
+    hostname_to_domain_id = {
+        "payload0": 0,
+        "payload1": 1,
+        "payload2": 2,
+        "payload3": 3,
+        "payload4": 4,
+    }
+    domain_id = hostname_to_domain_id.get(computer_hostname, -1)
+    if domain_id == -1:
+        raise RuntimeError(
+            f"Unknown hostname {computer_hostname}, " "do not know ROS_DOMAIN_ID."
+        )
+    return domain_id
+
+
 def generate_launch_description():
     # Declare launch argument for optional rosbag recording
     record_rosbag_arg = DeclareLaunchArgument(
@@ -164,12 +182,21 @@ def generate_launch_description():
         condition=IfCondition(record_rosbag),
     )
 
+    # set ZENOH parameters
+    zenoh_env = SetEnvironmentVariable(
+        name="ZENOH_CONFIG_OVERRIDE",
+        value="transport/link/tx/queue/congestion_control/drop/wait_before_drop=1000000",
+    )
+
+    # Set ROS_DOMAIN_ID based on the computer hostname
+    ros_domain_id = SetEnvironmentVariable(
+        name="ROS_DOMAIN_ID", value=str(get_domain_id())
+    )
+
     return LaunchDescription(
         [
-            SetEnvironmentVariable(
-                name="ZENOH_CONFIG_OVERRIDE",
-                value="transport/link/tx/queue/congestion_control/drop/wait_before_drop=1000000",
-            ),
+            zenoh_env,
+            ros_domain_id,
             record_rosbag_arg,
             bag_name_arg,
             rmw_zenohd_process,
