f5f1007e03fed1c6d11bb55f115b0decb024feda
bhandare.aj <bhandare.aj@northeastern.edu>
2025-08-07 17:13:38 -0400

    moved changes from global to calib launch file

--- launch/calib_launch.py
+++ launch/calib_launch.py
@@ -6,13 +6,29 @@ from launch.actions import (
     DeclareLaunchArgument,
     SetEnvironmentVariable,  # Added for cyclonedds
 )
+from launch_ros.actions import Node
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
+COMPUTER_HOSTNAME = os.uname()[1]
+HOSTNAME_TO_DOMAIN_ID = {
+    "payload0": 0,
+    "payload1": 1,
+    "payload2": 2,
+    "payload3": 3,
+    "payload4": 4,
+}
+if COMPUTER_HOSTNAME not in HOSTNAME_TO_DOMAIN_ID:
+    raise RuntimeError(
+        f"Unknown hostname {COMPUTER_HOSTNAME}, should be one of: {list(HOSTNAME_TO_DOMAIN_ID.keys())}"
+    )
 
+import datetime
+now = datetime.datetime.now()
+BAG_FNAME = f"{COMPUTER_HOSTNAME}_{now.strftime('%Y%m%d_%H%M')}"
 def generate_launch_description():
     # Declare launch argument for optional rosbag recording
     record_rosbag_arg = DeclareLaunchArgument(
@@ -21,7 +37,14 @@ def generate_launch_description():
         description='Set to true to record rosbag'
     )
 
+    bag_name_arg = DeclareLaunchArgument(
+        "bag_name",
+        default_value=BAG_FNAME,
+        description="Name for the rosbag file (without extension)",
+    )
     record_rosbag = LaunchConfiguration('record_rosbag')
+
+    bag_name = LaunchConfiguration("bag_name")
 
     # Start rmw_zenohd daemon
     rmw_zenohd_process = ExecuteProcess(
@@ -71,13 +94,19 @@ def generate_launch_description():
             ),
         ]
     )
-
+    
+    downsampler_node = Node(
+        package = 'auxiliary',
+        executable='downsampler'
+    )
     # List of topics to record (cleaned, no leading space)
     rosbag_topics = [
     '/vectornav/imu',
     '/cam_sync/cam0/image_raw', 
     '/cam_sync/cam1/image_raw',
     '/ouster/points',
+    "/downsampled_imgs/cam0/image_raw",
+    "/downsampled_imgs/cam1/image_raw",
     ]
 
     rosbag_record = TimerAction(
@@ -85,13 +114,23 @@ def generate_launch_description():
         actions=[
             ExecuteProcess(
                 cmd=[
-                    'ros2', 'bag', 'record', '-s', 'mcap',
-                    '-o', '/home/roam1/data/calib_bag', '--max-cache-size', '2147483648', '--storage-preset-profile', 'fastwrite',
-                ] + rosbag_topics,
-                output='screen'
+                    "ros2",
+                    "bag",
+                    "record",
+                    "-s",
+                    "mcap",
+                    "-o",
+                    PathJoinSubstitution(["/home/neuroam/data/", bag_name]),
+                    "--max-cache-size",
+                    "6442450944",
+                    "--storage-preset-profile",
+                    "fastwrite",
+                ]
+                + rosbag_topics,
+                output="screen",
             )
         ],
-        condition=IfCondition(record_rosbag)
+        condition=IfCondition(record_rosbag),
     )
 
     def get_domain_id():
@@ -126,8 +165,10 @@ def generate_launch_description():
         zenoh_env,
         ros_domain_id,
         record_rosbag_arg,
+        bag_name_arg,
         rmw_zenohd_process,
         delayed_launch,
-        rosbag_record
+        rosbag_record,
+        downsampler_node,
     ])
 
